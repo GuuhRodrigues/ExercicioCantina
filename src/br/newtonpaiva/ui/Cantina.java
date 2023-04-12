aaca1d1785993d42b24afc8dc117240f2a2868f9
12116534 <guhrodriguesads@gmail.com>
2023-04-11 22:06:50 -0300

    feat: trata exceções para valor da massa

--- src/br/newtonpaiva/ui/Cantina.java
+++ src/br/newtonpaiva/ui/Cantina.java
@@ -13,64 +13,67 @@ import java.util.List;
 public class Cantina {
     public static void main(String[] args) {
 
-        String nomeC, nomeM;
-        double valorM;
+        String nomeC = "" , nomeM = "";
+        double valorM = 0.0;
         int opcao = 0;
-
 
         nomeC = JOptionPane.showInputDialog("Informe o nome do cliente: ");
         nomeM = JOptionPane.showInputDialog("Informe o nome da massa: ");
-        valorM = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor da massa: "));
 
-        Cliente c1 = new Cliente();
-        c1.setNomeCliente(nomeC);
-        Massa m1 = new Massa();
-        m1.setTipoMassa(nomeM);
-        m1.setValorMassa(valorM);
-        Pedido p1 = new Pedido();
-        p1.setLista(new ArrayList<>());
+        try {
+            valorM = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor da massa: "));
 
-        while (opcao != 5){
-            opcao = Integer.parseInt(JOptionPane.showInputDialog("Informe os ingrediente escolhidos:\n1- Bacon\n2- Mussarela\n3- Tomate\n4- Queijo\n5- Sair"));
-            switch (opcao){
-                case 1:
-                    Ingrediente i1 = new Ingrediente();
-                    i1.setNomeIngrediente("Bacon");
-                    p1.getLista().add(i1);
-                    break;
-                case 2:
-                    Ingrediente i2 = new Ingrediente();
-                    i2.setNomeIngrediente("Mussarela");
-                    p1.getLista().add(i2);
-                    break;
-                case 3:
-                    Ingrediente i3 = new Ingrediente();
-                    i3.setNomeIngrediente("Tomate");
-                    p1.getLista().add(i3);
-                    break;
-                case 4:
-                    Ingrediente i4 = new Ingrediente();
-                    i4.setNomeIngrediente("Queijo");
-                    p1.getLista().add(i4);
-                    break;
+            Cliente c1 = new Cliente();
+            c1.setNomeCliente(nomeC);
+            Massa m1 = new Massa();
+            m1.setTipoMassa(nomeM);
+            m1.setValorMassa(valorM);
+            Pedido p1 = new Pedido();
+            p1.setLista(new ArrayList<>());
+
+            while (opcao != 5) {
+                opcao = Integer.parseInt(JOptionPane.showInputDialog("Informe os ingrediente escolhidos:\n1- Bacon\n2- Mussarela\n3- Tomate\n4- Queijo\n5- Sair"));
+                switch (opcao) {
+                    case 1:
+                        Ingrediente i1 = new Ingrediente();
+                        i1.setNomeIngrediente("Bacon");
+                        p1.getLista().add(i1);
+                        break;
+                    case 2:
+                        Ingrediente i2 = new Ingrediente();
+                        i2.setNomeIngrediente("Mussarela");
+                        p1.getLista().add(i2);
+                        break;
+                    case 3:
+                        Ingrediente i3 = new Ingrediente();
+                        i3.setNomeIngrediente("Tomate");
+                        p1.getLista().add(i3);
+                        break;
+                    case 4:
+                        Ingrediente i4 = new Ingrediente();
+                        i4.setNomeIngrediente("Queijo");
+                        p1.getLista().add(i4);
+                        break;
+                }
             }
+            String mensagem = "";
+            int posicao = 0;
+
+            for (Ingrediente item : p1.getLista()) {
+                if (posicao == p1.getLista().size() - 2) {
+                    mensagem += item.getNomeIngrediente() + " e ";
+                } else if (posicao == p1.getLista().size() - 1) {
+                    mensagem += item.getNomeIngrediente() + ".";
+                } else {
+                    mensagem += item.getNomeIngrediente() + ", ";
+                }
+                posicao++;
+            }
+            p1.calculaValor(m1);
+            JOptionPane.showMessageDialog(null, "O pedido feito para " + c1.getNomeCliente() + "\n" + m1.getTipoMassa() + " com " + mensagem + "\nValor:  " + p1.getValorTotal(), "Informação", JOptionPane.INFORMATION_MESSAGE);
         }
-        String mensagem = "";
-        int posicao = 0;
-
-        for (Ingrediente item : p1.getLista()){
-            if (posicao == p1.getLista().size() - 2){
-                mensagem += item.getNomeIngrediente() + " e ";
-            }
-            else if (posicao == p1.getLista().size() - 1){
-                mensagem += item.getNomeIngrediente() + ".";
-            }
-            else {
-                mensagem += item.getNomeIngrediente() + ", ";
-            }
-            posicao ++;
+        catch (Exception e1){
+            JOptionPane.showMessageDialog(null, "Erro: Você não pode digitar letras");
         }
-        p1.calculaValor(m1);
-        JOptionPane.showMessageDialog(null, "O pedido feito para " + c1.getNomeCliente() + "\n" + m1.getTipoMassa() + " com " + mensagem + "\nValor:  " + p1.getValorTotal(), "Informação", JOptionPane.INFORMATION_MESSAGE);
     }
 }
